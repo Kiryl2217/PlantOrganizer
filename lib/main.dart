3107cf506fc9f7f39095f1bf23e954ac353cafb0
Kiryl2217 <ramemkelvin@gmail.com>
2026-09-16 00:04:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'views/main_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('ru', null);
   runApp(const PlantOrganizerApp());
 }
 
